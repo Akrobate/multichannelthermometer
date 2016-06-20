535dad1c046d31269ca75ce70f8bce3e50dbe6dd
Artiom FEDOROV <fedorov.artiom@gmail.com>
2016-06-20 15:36:20 +0200

    Update OsApplication.cpp
    fixes

--- OsApplication.cpp
+++ OsApplication.cpp
@@ -16,14 +16,13 @@ OsApplication::OsApplication() {
 
 void OsApplication::setLCDDevice(LiquidCrystal * _plcd) {
   render->setLCD(_plcd);
-  render->display("Super Fuck");
 }
 
 
 void OsApplication::init() {
 
-  
 }
+
 
 void OsApplication::setProbesPins(int pins[NBR_INPUT_ANALOG_PINS]) {
   thermometer->setPinConnection(pins);
